3c66964b3e9ca2f39416cef6b83c45a8c0ed5a28
KimJuheee <3098k@naver.com>
2024-05-23 20:45:48 +0900

    error 해결

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <MainComponent />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/Header.vue';
+import MainComponent from './components/Main.vue';
+import FooterComponent from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  text-align: center;
+}
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <MainComponent />
+  </div>
+</template>
+
+<script>
+import MainComponent from '../components/Main.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    MainComponent,
+  },
+};
+</script>
